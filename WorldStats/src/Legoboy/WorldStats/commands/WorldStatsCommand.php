3f3ee6f54c02a4d997b02a8a8417479ff59fad71
legoboy0215 <legoboy0215weebly@gmail.com>
2015-08-26 18:59:24 +0800

    Update WorldStatsCommand.php

--- WorldStats/src/Legoboy/WorldStats/commands/WorldStatsCommand.php
+++ WorldStats/src/Legoboy/WorldStats/commands/WorldStatsCommand.php
@@ -31,22 +31,6 @@ class WorldStatsCommand extends Command implements PluginIdentifiableCommand{
         $sender->sendMessage("§a/worldstats <update|info>");
     }
     
-    private function updateSigns(){
-        foreach($this->plugin->getServer()->getLevels() as $levels){
-            foreach($levels->getTiles() as $tiles){
-                if($tiles instanceof Sign){
-                    $text = $tiles->getText();
-                    if(TextFormat::clean(strtolower(trim($text[0]))) === strtolower(trim($this->plugin->sign->get("sign_final_change")))){
-		        $world = $text[1];
-			$level = $this->plugin->getServer()->getLevelByName($world);
-			$players = count($level->getPlayers());
-			$tiles->setText($text[0] = TextFormat::GREEN . $this->plugin->sign->get("sign_final_change"), $text[2] = TextFormat::YELLOW . $players . " players");
-		    }
-                }
-            }
-        }
-    }
-    
     public function execute(CommandSender $sender, $label, array $args){
         if(isset($args[0])){
             switch(strtolower($args[0])){
