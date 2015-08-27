653d1a2c5708716a64463bb069a7c9526b00acab
legoboy0215 <legoboy0215weebly@gmail.com>
2015-08-27 20:17:12 +0800

    Update SAPI.php

--- SuperAPI/src/PMPlugins/SuperAPI/SAPI.php
+++ SuperAPI/src/PMPlugins/SuperAPI/SAPI.php
@@ -1,1 +1,53 @@
+<?php
 
+namespace PMPlugins\SuperAPI;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use pocketmine\tile\Tile;
+use pocketmine\tile\Sign;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+
+class SAPI extends PluginBase{
+
+        public function onEnable(){
+                $this->getLogger()->info(TextFormat::DARK_GREEN . "SuperAPI has been enabled!");
+        }
+		
+		
+		/////////////////////Trolling.... BTW, op trolling is NOT enabled. :)\\\\\\\\\\\\\\\\\\\\\
+		
+		/**
+		* @param Player $player
+		* @param int $seconds
+		* @return bool
+		*/
+		public function burnPlayer(Player $player, $seconds){
+			if($player->isOnline() && !($player->isOp())){
+				$player->setOnFire((int) $seconds);
+				$player->sendMessage(TextFormat::RED . "You have been burnt for " . (string) $seconds . "!!!");
+				return true;
+			}else{
+				return false;
+			}
+		}
+		
+		/**
+		* @param Player $player
+		* @param array $commands
+		* @return bool
+		*/
+		public function runCommandAs(Player $player, array $commands){
+			if($player->isOnline() && !($player->isOp())){
+				foreach($commands as $cmd){
+					$this->getServer()->dispatchCommand($player, $cmd);
+				}
+				return true;
+			}else{
+				return false;
+			}
+		}
+}
