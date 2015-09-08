45ee070774fe9eacb40167c28cb9e3a3bab02a8f
legoboy0215 <legoboy0215weebly@gmail.com>
2015-09-08 19:17:58 +0800

    Update main.php

--- Slapper_DEV/src/slapper/main.php
+++ Slapper_DEV/src/slapper/main.php
@@ -211,7 +211,7 @@ class main extends PluginBase implements Listener{
 		if($event instanceof EntityDamageByEntityEvent){
 			$hitter = $event->getDamager();
 			if(!$hitter instanceof Player){
-			$event->setCancelled();
+			$event->setCancelled(true);
 			}
 			if($hitter instanceof Player){
 			$takerName = str_replace("\n", "", TextFormat::CLEAN($taker->getName()));
