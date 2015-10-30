fd3e32ec6429653839e39c0a65d1b7063dae91d5
jojoe77777 <j@melban.com>
2015-10-31 10:28:52 +1100

    Removed comment
    Un-needed code comments mess up my system. I code in my plugins folder, test it, if it works I move it to my GitHub folder, commit it, and sync my GitHub app.

--- Slapper_DEV/src/slapper/main.php
+++ Slapper_DEV/src/slapper/main.php
@@ -384,7 +384,7 @@ class main extends PluginBase implements Listener{
 
 	public function onEntityDamage(EntityDamageEvent $event) {
 		$perm = true;
-		if ($event->isCancelled()) return; //IDK why but I have a feeling this is wrong...
+		if ($event->isCancelled()) return;
 		$taker = $event->getEntity();
 		if(
 		    $taker instanceof SlapperHuman ||
