62c1f687fb38d09873619401a85ff9c3da464bb8
Test <test@test.com>
2016-01-01 11:27:43 +1100

    Updated README.md

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 > This is where we upload code under development!
 --- <b>jojoe77777 & Legoboy0215</b>
 
-<h3>Plugin Status:</h3>
+<h3>Plugin Statuses:</h3>
 |Name|Version|Author|Active?|Completed?|Status|
 |----|:-------:|:------:|:-------:|:----------:|:------:|
 |Slapper|1.2.8|jojoe77777|Yes|No|May crash|
@@ -13,5 +13,4 @@
 |SignTools|1.0.0|Legoboy0215|Yes|No|Does not work|
 |SuperAPI|1.0.0|Legoboy0215|Yes|No|Works|
 |TellRaw|1.0.3|jojoe77777|Yes|Yes|Works|
-|<i>Lookout(Abandoned)</i>|1.0.0|Legoboy0215|Yes|Half|Timer works, but not tested functions.|
 |Placeholders|1.0.2|jojoe77777|No|No|Should work|
--- Slapper_DEV/src/slapper/entities/SlapperSilverfish.php
+++ Slapper_DEV/src/slapper/entities/SlapperSilverfish.php
@@ -1,42 +1,42 @@
 <?php
 namespace slapper\entities;
 
-use pocketmine\network\protocol\AddEntityPacket;
-use pocketmine\network\Network;
-use pocketmine\Player;
 use pocketmine\entity\Entity;
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\Player;
 
-class SlapperSilverfish extends Entity{
+class SlapperSilverfish extends Entity
+{
 
-	const NETWORK_ID = 39;
+    const NETWORK_ID = 39;
 
-	public function getName(){
-		return $this->getDataProperty(2);
-	}
+    public function getName()
+    {
+        return $this->getDataProperty(2);
+    }
 
-	public function spawnTo(Player $player){
+    public function spawnTo(Player $player)
+    {
 
-		$pk = new AddEntityPacket();
-		$pk->eid = $this->getId();
-		$pk->type = self::NETWORK_ID;
-		$pk->x = $this->x;
-		$pk->y = $this->y;
-		$pk->z = $this->z;
-		$pk->speedX = 0;
-		$pk->speedY = 0;
-		$pk->speedZ = 0;
-		$pk->yaw = $this->yaw;
-		$pk->pitch = $this->pitch;
-		$pk->metadata = [
-			2 => [4, $this->getDataProperty(2)],
-			3 => [0, $this->getDataProperty(3)],
-			15 => [0, 1]
-		];
-		$player->dataPacket($pk);
-		parent::spawnTo($player);
-	}
-
-
+        $pk = new AddEntityPacket();
+        $pk->eid = $this->getId();
+        $pk->type = self::NETWORK_ID;
+        $pk->x = $this->x;
+        $pk->y = $this->y;
+        $pk->z = $this->z;
+        $pk->speedX = 0;
+        $pk->speedY = 0;
+        $pk->speedZ = 0;
+        $pk->yaw = $this->yaw;
+        $pk->pitch = $this->pitch;
+        $pk->metadata = [
+            2 => [4, $this->getDataProperty(2)],
+            3 => [0, $this->getDataProperty(3)],
+            15 => [0, 1]
+        ];
+        $player->dataPacket($pk);
+        parent::spawnTo($player);
+    }
 
 
 }
