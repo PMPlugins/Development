353b0d246b51b1a1984665cb12f4cd202d7f52c0
TheNewHEROBRINEX <TheNewHEROBRINEX@users.noreply.github.com>
2016-03-22 00:41:23 +0100

    Update Main.php

--- ArrowTP/src/jojoe77777/arrowtp/Main.php
+++ ArrowTP/src/jojoe77777/arrowtp/Main.php
@@ -28,7 +28,8 @@ class Main extends PluginBase implements Listener
     public function onArrowHit(ProjectileHitEvent $ev){
         $e = $ev->getEntity();
         $s = $e->shootingEntity;
-        if($e instanceof Arrow && $s instanceof Player && in_array(strtolower($e->getLevel()->getName()), $this->getConfig()->get("worlds")))
-                $s->teleport($e->getPosition(), $s->getYaw(), $s->getPitch());
-            }
+        if($e instanceof Arrow && $s instanceof Player && in_array(strtolower($e->getLevel()->getName()), $this->getConfig()->get("worlds"))){
+            if($s->hasPermission("arrowtp.tp"))
+            $s->teleport($e->getPosition(), $s->getYaw(), $s->getPitch());
+        }
 }
