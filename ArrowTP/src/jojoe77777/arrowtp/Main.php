5be6ce5e8388baa0fc52da4946a3bf9031b82e90
TheNewHEROBRINEX <TheNewHEROBRINEX@users.noreply.github.com>
2016-03-22 15:10:11 +0100

    Update Main.php

--- ArrowTP/src/jojoe77777/arrowtp/Main.php
+++ ArrowTP/src/jojoe77777/arrowtp/Main.php
@@ -9,27 +9,23 @@ use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase implements Listener
-{
+class Main extends PluginBase implements Listener{
 
-    public function onEnable()
-    {
+    public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveDefaultConfig();
         $this->getLogger()->info(TextFormat::GREEN . "ArrowTP by jojoe77777 has been enabled!");
-
     }
 
-    public function onDisable()
-    {
+    public function onDisable(){
         $this->getLogger()->info(TextFormat::GREEN . "ArrowTP by jojoe77777 has been disabled.");
     }
 
     public function onArrowHit(ProjectileHitEvent $ev){
         $e = $ev->getEntity();
         $s = $e->shootingEntity;
-        if($e instanceof Arrow && $s instanceof Player && in_array(strtolower($e->getLevel()->getName()), $this->getConfig()->get("worlds"))){
+        if($e instanceof Arrow && $s instanceof Player && in_array(strtolower($e->getLevel()->getName()), $this->getConfig()->get("worlds")))
             if($s->hasPermission("arrowtp.tp"))
-            $s->teleport($e->getPosition(), $s->getYaw(), $s->getPitch());
-        }
+                $s->teleport($e->getPosition(), $s->getYaw(), $s->getPitch());
+    }    
 }
